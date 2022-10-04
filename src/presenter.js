6a9ca557f5ed15469ac2fc9c215ec210bd21429a
younglee <young.lee@ucb.edu.bo>
2022-10-04 01:00:36 -0400

    html

---
+++ src/presenter.js
@@ -0,0 +1,18 @@
+//import ejecutarComando from "./torosyvacas.js";
+
+//const comando = document.querySelector("#idcomandos");
+
+//const cD = new ejecutarComando();
+//const form = document.querySelector("#simulador-form");
+//const div = document.querySelector("#resultado-div");
+const iniForm = document.querySelector("#inicio-form");
+const juegoForm = document.querySelector("#inicio-form");
+
+
+iniForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  iniForm.innerHTML = "";
+});
+
+//<input type="text" id="idintento" />
---
+++ src/torosyvacas.spec.js
@@ -0,0 +1,25 @@
+/*import ejecutarComando from "./Auto";
+
+const cD = new ejecutarComando();
+describe("Ejecutar comandos", () => {
+  
+    it("Avanzar", () => {
+      expect(cD.ejecutarComando("A")).toEqual("0,1N");
+    });
+    it("Girar Derecha", () => {
+      expect(cD.ejecutarComando("D")).toEqual("0,1E");
+    });
+    it("Girar Izquierda", () => {
+      expect(cD.ejecutarComando("I")).toEqual("0,1N");
+    });
+    it("Avanzar mas de una vez", () => {
+      expect(cD.ejecutarComando("AAA")).toEqual("0,4N");
+    });
+    it("Realizar multiples movimientos y giros", () => {
+      expect(cD.ejecutarComando("DDAAAIADADAD")).toEqual("0,0N");
+    });
+    it("Realizar movimientos validos sin desbordarse del plano 10x10", () => {
+      expect(cD.ejecutarComando("IADAAAAAAAAAADAAAAAAAAAAADAAAAAAAAAA")).toEqual("9,0S");
+    });
+  });*/
+  
